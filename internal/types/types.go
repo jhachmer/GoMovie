085b07f5f6158c92eeb7130e4511d94805f6db7a
Jannes <jannes.hachmer@gmail.com>
2024-12-22 00:25:15 +0100

    added InfoPage constructor function

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,4 +1,4 @@
-package media
+package types
 
 // Rating holds rating data, which are pairs of Source and the actual rating value
 type Rating struct {
@@ -68,4 +68,37 @@ func NewMovieFromTitleAndYear(title, year string) (*Movie, error) {
 		return nil, err
 	}
 	return mov, nil
+}
+
+// NewInfoPage returns pointer to a new InfoPage instance
+func NewInfoPage(mov *Movie, entries []*Entry) *InfoPage {
+	return &InfoPage{
+		Entries: entries,
+		Movie:   mov,
+	}
+}
+
+// InfoPage holds necessary data for the InfoHandler
+type InfoPage struct {
+	Entries []*Entry
+	Movie   *Movie
+}
+
+// Entry holds data regarding user submitted info
+// e.g. username, if the movie is already watched and a comment
+type Entry struct {
+	ID      int64
+	Name    string
+	Watched bool
+	Comment []byte
+}
+
+// NewEntry returns pointer to a new Entry instance
+func NewEntry(name string, watched bool, comment string) *Entry {
+	return &Entry{
+		ID:      0,
+		Name:    name,
+		Watched: watched,
+		Comment: []byte(comment),
+	}
 }
