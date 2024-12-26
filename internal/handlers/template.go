e35306311a9bc713b925a40605563b32d62cf60c
Jannes <jannes.hachmer@gmail.com>
2024-12-26 02:20:36 +0100

    moved parsing of templates out of var declaration
    before tests would explode because of path issues

--- internal/handlers/template.go
+++ internal/handlers/template.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles("./templates/index.gohtml", "./templates/info.gohtml", "./templates/overview.gohtml"))
+var templates *template.Template
+
+func InitTemplates() {
+	templates = template.Must(template.ParseFiles("./templates/index.gohtml", "./templates/info.gohtml", "./templates/overview.gohtml", "./templates/movie-grid.gohtml"))
+}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, d any) {
 	err := templates.ExecuteTemplate(w, tmpl+".gohtml", d)
