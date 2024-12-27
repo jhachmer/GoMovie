e3805331be1cf377cba9aa4d9ee5203490c91a87
Jannes <jannes.hachmer@gmail.com>
2024-12-27 03:13:50 +0100

    added a dedicated search bar for not yet added movies

---
+++ templates/scripts/newinfo.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('menu-search-bar').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const searchInput = document.getElementById('search-input').value.trim();
+
+        const imdbIdPattern = /^tt\d{7,8}$/;
+        console.log('Test JS Loaded');
+
+        if (imdbIdPattern.test(searchInput)) {
+            window.location.href = `/films/${searchInput}`;
+        } else {
+            window.location.href = `/search?q=${encodeURIComponent(searchInput)}`;
+        }
+    });
+});
